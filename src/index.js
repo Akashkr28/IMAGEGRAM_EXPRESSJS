ab1b33a33b491a753aee502cfa5e9bf7f4215a2c
Akashkr28 <email.akash2010@gmail.com>
2024-10-12 12:57:30 +0530

    added controller

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
+import { createPost } from './controllers/postController.js';
 
 const PORT = 3000; //port number
 
 const app = express(); // create express app server instance
 
+app.use(express.json()); // parse incoming requests with JSON payloads
+app.use(express.text());
+app.use(express.urlencoded());
+
+function m1(req, res, next) {
+    console.log('m1');
+    next();
+}
+
+app.use(m1);
+
 app.get('/ping', (req, res) => {
     return res.json({ message: 'pong' });
 });
+
+app.post('/post', createPost);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
