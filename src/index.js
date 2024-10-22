1bb7a5cb3563eb25589d0455643131f79047b708
Akashkr28 <email.akash2010@gmail.com>
2024-10-23 04:54:54 +0530

    Added routing layer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
-import { createPost } from './controllers/postController.js';
-import { s3uploader } from './config/multerconfig.js';
+import apiRouter from './routers/apiRouter.js'
 
 const PORT = 3000; //port number
 
@@ -10,6 +9,8 @@ const app = express(); // create express app server instance
 app.use(express.json()); // parse incoming requests with JSON payloads
 app.use(express.text());
 app.use(express.urlencoded());
+
+app.use('/api', apiRouter); // If the url starts with /api then the request is forwaded to the apiRouter
 
 function m1(req, res, next) {
     console.log('m1');
@@ -22,7 +23,6 @@ app.get('/ping', (req, res) => {
     return res.json({ message: 'pong' });
 });
 
-app.post('/post', s3uploader.single('image'), createPost);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
