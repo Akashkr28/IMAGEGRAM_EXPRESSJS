758956aed396104b77ccc817bc5912f35fff26a2
Akashkr28 <email.akash2010@gmail.com>
2024-11-10 23:53:43 +0530

    signup and user signin error resolved

--- src/schema/user.js
+++ src/schema/user.js
@@ -19,24 +19,22 @@ const userSchema = new mongoose.Schema({
             },
             message: 'Please enter a valid email address'
         },
-        password: {
+    },
+    password: {
             type: String,
             required: true,
             minLength: 5
         }
-    }
 }, { timestamps: true });
 
 userSchema.pre('save', function modifyPassword(next) {
     // incoming user object
-    const user = this; //object with plain password
-
+    const user = this; // object with plain password
     const SALT = bcrypt.genSaltSync(9);
     // hash password
     const hashedPassword = bcrypt.hashSync(user.password, SALT);
     // replace plain password with hashed password
     user.password = hashedPassword;
-
     next();
 });
 
